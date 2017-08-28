03780397d4b124d5497a0261bc3738096b9d2ed0
Jason Garman <jgarman@carbonblack.com>
2017-08-28 13:15:35 -0400

    rev version to 0.9.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='cbint',
-    version='0.8.11',
+    version='0.9.0',
     url='https://developer.carbonblack.com/',
     license='MIT',
     author='Carbon Black',
