1d7aab88d3afaf1614eea31b73889a36cd7c3397
Zachary Estep <zestep@carbonblack.com>
2020-10-28 14:47:51 -0400

    Update daemon.py

--- src/cbint/utils/daemon.py
+++ src/cbint/utils/daemon.py
@@ -14,6 +14,7 @@ import errno
 import traceback
 import cbint.utils.filesystem
 from netifaces import interfaces, ifaddresses, AF_INET6, AF_INET, gateways
+from io import open as file
 
 log = logging.getLogger(__name__)
 
